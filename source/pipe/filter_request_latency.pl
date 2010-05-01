5027f900d8d5a5d2e8a8a27da5bbb1eb9697ad11
lianghon <>
2010-05-01 07:54:15 +0000

    filter_request_latency.pl now generates two more output files: cluster_rank_by_frequency.dat:	cluster distribution ranked by request frequency cluster_rank_by_latency.dat:	cluster distribution ranked by average request latency

--- source/pipe/filter_request_latency.pl
+++ source/pipe/filter_request_latency.pl
@@ -1,6 +1,6 @@
 #! /usr/bin/perl -w
 
-# $cmuPDL: filter_request_latency.pl,v 1.5 2010/04/25 05:05:38 lianghon Exp $v
+# $cmuPDL: filter_request_latency.pl,v 1.1.2.1 2010/04/25 17:52:48 lianghon Exp $v
 
 ##
 # This program calculates the cluster distribution within a specific request 
@@ -30,7 +30,10 @@ my $min_latency = 0;
 my $max_latency = 100000;
 my $input_file;
 my $output_file;
-my %cluster_hash = ();
+my $rank_by_latency_file;
+my $rank_by_frequency_file;
+my %frequency_hash = ();
+my %latency_hash = ();
 
 ##### Functions ########
 
@@ -66,11 +69,17 @@ parse_options();
 
 $input_file = "$g_output_dir/convert_data/global_ids_to_cluster_ids.dat";
 $output_file = "cluster_distribution.dat";
+$rank_by_latency_file = "cluster_rank_by_latency.dat";
+$rank_by_frequency_file = "cluster_rank_by_frequency.dat";
 
 open(my $input_fh, "<$input_file")
     or die("Could not open $input_file\n");
 open(my $output_fh, ">$output_file")
     or die("Could not open $output_file\n");
+open(my $latency_fh, ">$rank_by_latency_file")
+    or die("Could not open $rank_by_latency_file\n");
+open(my $frequency_fh, ">$rank_by_frequency_file")
+    or die("Could not open $rank_by_frequency_file\n");
 
 while(<$input_fh>) {
     chomp;
@@ -83,17 +92,39 @@ while(<$input_fh>) {
 	if($req_latency >= $min_latency &&
 	   $req_latency <= $max_latency) {
 
-	    if(!defined $cluster_hash{$cluster_id}) {
-		$cluster_hash{$cluster_id} = 1;
+	    if(!defined $frequency_hash{$cluster_id}) {
+		$frequency_hash{$cluster_id} = 1;
 	    } else {
-		$cluster_hash{$cluster_id}++;
+		$frequency_hash{$cluster_id}++;
+	    }
+
+	    if(!defined $latency_hash{$cluster_id}) {
+		$latency_hash{$cluster_id} = $req_latency;
+	    } else {
+		$latency_hash{$cluster_id} += $req_latency;
 	    }
 	}
     }
 }
 
-for my $key (sort {$a<=>$b} keys %cluster_hash) {
-    printf $output_fh "$key $cluster_hash{$key}\n";
+# calculate average request latency for each cluster
+for my $cid (keys %latency_hash) {
+    $latency_hash{$cid} /= $frequency_hash{$cid};
+}
+
+# output file ranked by cluster IDs
+for my $key (sort {$a<=>$b} keys %frequency_hash) {
+    printf $output_fh "$key $frequency_hash{$key}\n";
+}
+
+# output file ranked by latency
+for my $key (sort {$latency_hash{$a}<=>$latency_hash{$b}} keys %latency_hash) {
+    printf $latency_fh "$key $latency_hash{$key}\n";
+}
+
+# output file ranked by frequency
+for my $key (sort {$frequency_hash{$a}<=>$frequency_hash{$b}} keys %frequency_hash) {
+    printf $frequency_fh "$key $frequency_hash{$key}\n";
 }
 
 close($input_fh);
